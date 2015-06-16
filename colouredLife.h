23a51827e8c98acfa6539948612fe955ef5813ef
Troy Daniels <troy_daniels@live.com.au>
2015-06-16 19:58:37 +1000

    Tidy up files

--- colouredLife.h
+++
@@ -1,6 +0,0 @@
-void initBoard(int, int *);
-void printBoard(int, int *);
-void clearScreen(void);
-void updateBoard(int, int *);
-void waitFor(unsigned int);
-void runSimulation(int, int *);
--- gameOfLife.h
+++
@@ -1,5 +0,0 @@
-void initBoard(int, _Bool *);
-void printBoard(int, _Bool *);
-void clearScreen(void);
-void updateBoard(int, _Bool *);
-void waitFor(unsigned int);
